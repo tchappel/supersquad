0e72ab05f3850c6adf800e3df72edeb9ec0eb7a3
Thomas Chappel <thomas.chappel4@gmail.com>
2019-03-28 10:35:08 +0100

    add soundtrack

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import HeroesList from '../HeroesList'
 import {ColumnsWrapper, Column, ColumnTitle} from "./styled"
-import  SelectedHeroesList from '../SelectedHeroesList'
+import SelectedHeroesList from '../SelectedHeroesList'
 import SelectedHeroesTotals from '../SelectedHeroesTotals'
-
+import soundtrack from '../../assets/sounds/soundtrack.mp3'
 
 const App = () => {
     return (
-        <ColumnsWrapper>
-            <Column>
-                <ColumnTitle>Heroes</ColumnTitle>
-                <HeroesList/>
-            </Column>
-            <Column>
-                <ColumnTitle>Selected Heroes</ColumnTitle>
-                <SelectedHeroesList />
-            </Column>
-            <Column>
-                <ColumnTitle>Team Totals</ColumnTitle>
-                <SelectedHeroesTotals />
-            </Column>
-        </ColumnsWrapper>
+        <>
+            <ColumnsWrapper>
+                <Column>
+                    <ColumnTitle>Heroes</ColumnTitle>
+                    <HeroesList/>
+                </Column>
+                <Column>
+                    <ColumnTitle>Selected Heroes</ColumnTitle>
+                    <SelectedHeroesList/>
+                </Column>
+                <Column>
+                    <ColumnTitle>Team Totals</ColumnTitle>
+                    <SelectedHeroesTotals/>
+                </Column>
+            </ColumnsWrapper>
+            <audio src={soundtrack} loop="true" autoPlay="true" />
+        </>
     )
 }
 
